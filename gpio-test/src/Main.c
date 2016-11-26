c106d9d1880390f10cd89e3a3c2154265b2ac6c4
nils <nils.91@googlemail.com>
2016-11-26 13:51:35 +0100

    pin 0 inversion every 2 seconds

--- gpio-test/src/Main.c
+++ gpio-test/src/Main.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdio.h>
+#include <time.h>
 
 #include <wiringPi.h>
 
@@ -13,4 +14,10 @@ int main(int argc, char **argv) {
 	printf("Setting up wiringPi\n");
 	//setup wiring pi
 	wiringPiSetup();
+	//Set pin 0 as out
+	pinMode(0, OUTPUT);
+	while(1){
+		digitalWrite(0,!digitalRead(0)); //invert pin 0
+		sleep(2);
+	}
 }
